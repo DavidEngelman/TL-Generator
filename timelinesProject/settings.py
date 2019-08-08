aa552b80d28ae9ceb439139f589fa3eab372d282
Engelman David <daengelm@ulb.ac.be>
2019-08-08 21:04:14 +0200

    init commit

--- timelinesProject/settings.py
+++ timelinesProject/settings.py
@@ -19,11 +19,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9*m-u@dt%)fe7b#o&x=+bg_osapg5jh8z4=@@vs#!b@w2*0rpw'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '9*m-u@dt%)fe7b#o&x=+bg_osapg5jh8z4=@@vs#!b@w2*0rpw')
+
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
 
 ALLOWED_HOSTS = []
 
@@ -120,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
